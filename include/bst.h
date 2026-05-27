2729a93de1f1eb94a65d2b97b7822144764e090e
Edelweizznick <kolia3003@yahoo.com>
2026-05-27 13:10:07 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,87 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+#include <vector>
+#include <algorithm>
+#include <utility>
+
+template <typename T>
+struct Node {
+  T data;
+  int count;
+  Node* left;
+  Node* right;
+
+  Node(const T& val) : data(val), count(1), left(nullptr), right(nullptr) {}
+};
+
+template <typename T>
+class BST {
+ private:
+  Node<T>* root;
+
+  void destroy(Node<T>* node) {
+    if (node) {
+      destroy(node->left);
+      destroy(node->right);
+      delete node;
+    }
+  }
+
+  void insertHelper(Node<T>*& node, const T& val) {
+    if (!node) {
+      node = new Node<T>(val);
+    } else if (val < node->data) {
+      insertHelper(node->left, val);
+    } else if (val > node->data) {
+      insertHelper(node->right, val);
+    } else {
+      node->count++;
+    }
+  }
+
+  int searchHelper(Node<T>* node, const T& val) const {
+    if (!node) return 0;
+    if (node->data == val) return node->count;
+    if (val < node->data)
+      return searchHelper(node->left, val);
+    return searchHelper(node->right, val);
+  }
+
+  int depthHelper(Node<T>* node) const {
+    if (!node) return 0;
+    int leftD = depthHelper(node->left);
+    int rightD = depthHelper(node->right);
+    return std::max(leftD, rightD) + 1;
+  }
+
+  void collectHelper(Node<T>* node, std::vector<std::pair<T, int>>& vec) const {
+    if (node) {
+      collectHelper(node->left, vec);
+      vec.emplace_back(node->data, node->count);
+      collectHelper(node->right, vec);
+    }
+  }
+
+ public:
+  BST() : root(nullptr) {}
+  ~BST() { destroy(root); }
+
+  void insert(const T& val) {
+    insertHelper(root, val);
+  }
+
+  int search(const T& val) const {
+    return searchHelper(root, val);
+  }
+
+  int depth() const {
+    return depthHelper(root);
+  }
+
+  void collectFreq(std::vector<std::pair<T, int>>& vec) const {
+    collectHelper(root, vec);
+  }
+};
+
 #endif  // INCLUDE_BST_H_
